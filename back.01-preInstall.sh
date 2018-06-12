b5ea0bdfa6fb251a618592163fbb11b2800b8b6e
voroyam <voroyam@owncloud.com>
2018-06-12 17:15:16 +0200

    moved setup-ldap to docker image

--- back.01-preInstall.sh
+++ back.01-preInstall.sh
@@ -12,6 +12,7 @@ OWNCLOUD_BACKUP_DIR="${OWNCLOUD_DATA}/backup"
 echo "[01.PRE_INST] folder creation"
 mkdir -p $OWNCLOUD_CONF
 mkdir -p "$OWNCLOUD_DATA/files"
+touch /root/setup-ldap.sh
 
 echo "[01.PRE_INST] enable logging"
 to_logfile () {
@@ -110,12 +111,90 @@ then
   echo "[01.PRE_INST] adding performance tuning options"
   sed -i "s#'overwritewebroot' => '/owncloud',.*#&\n'ldapIgnoreNamingRules' => false, \n'filelocking.enabled' => 'false',\n 'htaccess.RewriteBase' => '/owncloud', \n 'integrity.check.disabled' => true, #" $OWNCLOUD_CONF/config.php
 
+  echo "[01.PRE_INST] generating pre-update config script"
+cat << EOF >| /root/setup-ldap.sh
+#!/usr/bin/env bash
+
+to_logfile () {
+  tee --append /var/lib/univention-appcenter/apps/owncloud/data/files/owncloud-appcenter.log
+}
+
+echo "[SETUP-LDAP] Fixing LDAP Settings"
+
+OWNCLOUD_PERMCONF_DIR="/var/lib/univention-appcenter/apps/owncloud/conf"
+OWNCLOUD_LDAP_FILE="\${OWNCLOUD_PERMCONF_DIR}/ldap"
+
+eval "\$(< \${OWNCLOUD_LDAP_FILE})"
+echo -e "\n\n------"
+cat \${OWNCLOUD_LDAP_FILE}
+
+
+echo "[SETUP-LDAP] Enable user_ldap app" 2>&1
+n=1
+until [ $n -ge 20 ]
+do 
+  r=$(occ app:enable user_ldap 2>&1)
+  t=$?
+  echo -n "."
+  [[ $t == 0 ]] && break
+  n=$(($n + 1))
+  sleep 1
+done
+echo
+#echo "enabling ldap user app in preinst script"
+#occ app:enable user_ldap
+
+echo "[SETUP-LDAP] set ldap config with values from variables"
+occ config:app:set user_ldap ldap_host --value="\${LDAP_MASTER}" 2>&1 | to_logfile
+occ config:app:get user_ldap ldap_host
+occ config:app:set user_ldap ldap_port --value="\${LDAP_MASTER_PORT}" 2>&1 | to_logfile
+occ config:app:get user_ldap ldap_port
+occ config:app:set user_ldap ldap_dn --value="\${LDAP_HOSTDN}" 2>&1 | to_logfile
+occ config:app:get user_ldap ldap_dn
+
+while ! test -f "/etc/machine.secret"; do
+  sleep 1 | to_logfile
+  echo "Still waiting"
+
+done
+
+# getting LDAP password and encoding it
+ldap_pwd_encoded=\$(cat /etc/machine.secret | base64 -w 0)
+echo \$ldap_pwd_encoded > ldap_pwd
+
+echo "[SETUP-LDAP] setting ldap password"
+occ config:app:set user_ldap ldap_agent_password --value="\$(cat ldap_pwd)" 2>&1 | to_logfile
+rm ldap_pwd
+
+echo "[SETUP-LDAP] setting ldap_base" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_base --value="\${owncloud_ldap_base}" 2>&1 | to_logfile
+occ config:app:get user_ldap ldap_base
+
+echo "[SETUP-LDAP] configure ldap" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_login_filter --value="\${owncloud_ldap_loginFilter}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_User_Filter --value="\${owncloud_ldap_userFilter}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_Group_Filter --value="\${owncloud_ldap_groupFilter}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_Quota_Attribute --value="\${owncloud_ldap_user_quotaAttribute}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_Expert_Username_Attr --value="\${owncloud_ldap_internalNameAttribute}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldap_Expert_UUID_User_Attr --value="\${owncloud_ldap_userUuid}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldapExpertUUIDGroupAttr --value="\${owncloud_ldap_groupUuid}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldapEmailAttribute --value="\${owncloud_ldap_internalNameAttribute}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldapGroupMemberAssocAttr --value="\${owncloud_ldap_memberAssoc}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldapBaseUsers --value="\${owncloud_ldap_base_users}" 2>&1 | to_logfile
+occ config:app:set user_ldap ldapBaseGroups --value="\${owncloud_ldap_base_groups}" 2>&1 | to_logfile
+occ config:app:set user_ldap useMemberOfToDetectMembership --value="0" 2>&1 | to_logfile
+occ config:app:set user_ldap ldapConfigurationActive --value="1" 2>&1 | to_logfile
+
+
+EOF
+
 else
   echo "[01.PRE_INST] no previous installation found"
 
 fi
 
 echo "[01.PRE_INST] Updating Icon Image for ownCloud docs"
+
 
 eval "$(ucr shell)"
 
